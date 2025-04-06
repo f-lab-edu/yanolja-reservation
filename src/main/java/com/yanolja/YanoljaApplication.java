369525d57f89ea148773ac69bf998f74daa61d30
woodk3268 <155120219+woodk3268@users.noreply.github.com>
2025-04-06 16:57:08 +0900

    Spring Boot 프로젝트 초기 설정

---
+++ src/main/java/com/yanolja/YanoljaApplication.java
@@ -0,0 +1,11 @@
+package com.yanolja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YanoljaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YanoljaApplication.class, args);
+    }
+} 
