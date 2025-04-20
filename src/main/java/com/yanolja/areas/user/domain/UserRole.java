680ee66f9eae868bc93adde1c47271d8250a0e1c
woodk3268 <155120219+woodk3268@users.noreply.github.com>
2025-04-20 09:46:53 +0900

    add: UserRole 클래스에 enum converter 추가

--- src/main/java/com/yanolja/areas/user/domain/UserRole.java
+++ src/main/java/com/yanolja/areas/user/domain/UserRole.java
@@ -1,5 +1,35 @@
 package com.yanolja.areas.user.domain;
 
-public enum UserRole {
-    USER, ADMIN
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.yanolja.common.enumcode.EntityEnumerable;
+import com.yanolja.common.enumcode.EntityEnumerableConverter;
+import lombok.AllArgsConstructor;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@AllArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum UserRole implements EntityEnumerable {
+    @Schema(description = "유저 권한")
+    USER("USER", "일반 사용자"),
+    ADMIN("ADMIN", "관리자");
+
+    private String type;
+    private String name;
+
+    @Override
+    public String getType() {
+        return this.type;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @jakarta.persistence.Converter
+    public static class Converter extends EntityEnumerableConverter<UserRole> {
+        public Converter() {
+            super(UserRole.class);
+        }
+    }
 } 
