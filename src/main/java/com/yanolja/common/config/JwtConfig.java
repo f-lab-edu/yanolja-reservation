77d80f53669c17940155bf024ea86a90c6257af0
woodk3268 <155120219+woodk3268@users.noreply.github.com>
2025-04-21 22:27:22 +0900

    add : JwtConfig lombok.setter import 문 추가

--- src/main/java/com/yanolja/common/config/JwtConfig.java
+++ src/main/java/com/yanolja/common/config/JwtConfig.java
@@ -1,6 +1,7 @@
 package com.yanolja.common.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
