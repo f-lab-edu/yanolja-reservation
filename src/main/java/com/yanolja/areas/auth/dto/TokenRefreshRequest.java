b0865ab664b7934a86900cdc14baeb8071225c52
woodk3268 <155120219+woodk3268@users.noreply.github.com>
2025-05-02 22:58:19 +0900

    add: DTO에 @Builder 추가

--- src/main/java/com/yanolja/areas/auth/dto/TokenRefreshRequest.java
+++ src/main/java/com/yanolja/areas/auth/dto/TokenRefreshRequest.java
@@ -2,10 +2,12 @@ package com.yanolja.areas.auth.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class TokenRefreshRequest {
