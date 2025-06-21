76856bf0f1645a9ff4f1f865590aa2d57b0ee82f
woodk3268 <155120219+woodk3268@users.noreply.github.com>
2025-06-21 16:16:54 +0900

    fix : 백엔드 enum 직렬화 방식 수정

--- src/main/java/com/yanolja/areas/user/domain/SocialProvider.java
+++ src/main/java/com/yanolja/areas/user/domain/SocialProvider.java
@@ -1,11 +1,9 @@
 package com.yanolja.areas.user.domain;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum SocialProvider {
     @Schema(description = "구글 로그인")
     GOOGLE("구글"),
@@ -14,7 +12,6 @@ public enum SocialProvider {
     KAKAO("카카오");
 
     private String label;
-
 
     public String getLabel() {
         return this.label;
