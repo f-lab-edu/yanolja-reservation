69817883cdcb6c5976bf9f626a04f24a21b6b7ba
woodk3268 <155120219+woodk3268@users.noreply.github.com>
2025-06-22 09:44:06 +0900

    add : RestTemplateConfig 설정 추가

---
+++ src/main/java/com/yanolja/config/RestTemplateConfig.java
@@ -0,0 +1,20 @@
+package com.yanolja.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout(Duration.ofSeconds(30)); // 30초
+
+        return new RestTemplate(factory);
+    }
+} 
